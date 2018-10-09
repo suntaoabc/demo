b6e91ac54a1fbbfe799e1c90112868eefad49674
suntaoabc <suntao4019@126.com>
2018-10-09 15:13:09 +0800

    demo

---
+++ demo-foo/src/main/java/com/github/suntao/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * ymm56.com Inc.
+ * Copyright (c) 2013-2018 All Rights Reserved.
+ */
+package com.github.suntao.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author suntao
+ * @date 2018-10-09 14:19
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Demo.class);
+        logger.info("test");
+    }
+}
