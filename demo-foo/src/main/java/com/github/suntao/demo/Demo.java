001e7957839c629429768a8463152a62fa3b72b1
tao.sun <tao.sunt@ymm56.com>
2018-11-06 18:20:28 +0800

    新增mybatis

--- demo-foo/src/main/java/com/github/suntao/demo/Demo.java
+++ demo-foo/src/main/java/com/github/suntao/demo/Demo.java
@@ -7,6 +7,9 @@ package com.github.suntao.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Calendar;
+import java.util.Objects;
+
 /**
  *
  * @author suntao
@@ -17,5 +20,11 @@ public class Demo {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Demo.class);
         logger.info("test");
+        int a = 30;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, -a);
+        System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+        System.out.println(Objects.equals(1, new Integer(1)));
     }
 }
